c473156766bec4f6754de693e8fc4b73e5c2f58b
ruthgabrielle <ruthfgms@gmail.com>
2022-03-14 22:16:21 -0300

    unit tests

---
+++ tests/unit/server/controller.test.js
@@ -0,0 +1,5 @@
+import { jest, expect, describe, test } from "@jest/globals";
+
+describe("Controller", () => {
+  test.todo("get file stream");
+});
---
+++ tests/unit/server/service.test.js
@@ -0,0 +1,7 @@
+import { jest, expect, describe, test } from "@jest/globals";
+
+describe("Service", () => {
+  test.todo('get file info')
+  test.todo('create file info')
+  test.todo('get file stream')
+});
